df3486539ba3bc50f077db71cc3d05610ec7dc9f
Håkon Bekken <haakon.bekken@hotmail.com>
2026-01-26 14:19:19 +0100

    software

---
+++ sp02/support_files/installation_files/me11_pegasus/ME11/sh_msbl_20210627/upload_MAX32664_fw.bat
@@ -0,0 +1,2 @@
+download_fw_over_i2c_host.exe -f MAX32664C_OS58_I2C_1PD_WHRM_AEC_SCD_WSPO2_Z_32.9.23.msbl -p "COM12" -d 4
+pause
---
+++ sp02/support_files/installation_files/me11_pegasus/Pegasus Host/flash_Max32630fast.bat
@@ -0,0 +1,3 @@
+openocdbinMaxim0.9\openocd -f .\max3263x_hdk.cfg -c "program MaximnFast-Host-1.6.0-rc2.bin verify reset exit"
+pause
+
